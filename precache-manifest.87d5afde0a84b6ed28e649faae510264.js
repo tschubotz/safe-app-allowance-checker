399f73998f14078e3bb3219dcb31d3cf7126f6b2
Tobias Schubotz <tobias.schubotz@gmail.com>
2020-11-09 10:21:49 +0100

    Updates

---
+++ precache-manifest.87d5afde0a84b6ed28e649faae510264.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "754027a1ceee46d917f70da149a92113",
+    "url": "/safe-app-allowance-checker/index.html"
+  },
+  {
+    "revision": "10c3c26da6916a245658",
+    "url": "/safe-app-allowance-checker/static/js/2.4373bbb4.chunk.js"
+  },
+  {
+    "revision": "825100165b05792dbd3bb607a46713c1",
+    "url": "/safe-app-allowance-checker/static/js/2.4373bbb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28bb45acfd6bec1a10a9",
+    "url": "/safe-app-allowance-checker/static/js/main.ce8fdc47.chunk.js"
+  },
+  {
+    "revision": "2c7d3838530b5b1e6c60",
+    "url": "/safe-app-allowance-checker/static/js/runtime-main.6088853e.js"
+  },
+  {
+    "revision": "47c7a29f39e976cbad1da973c9f6fc92",
+    "url": "/safe-app-allowance-checker/static/media/averta-bold.47c7a29f.woff2"
+  },
+  {
+    "revision": "30559e37a0ef8a886f814f588bfea868",
+    "url": "/safe-app-allowance-checker/static/media/averta-normal.30559e37.woff2"
+  }
+]);
